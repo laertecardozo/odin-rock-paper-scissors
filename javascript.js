c026644d18df7896c62c189bdf858c08346458a6
Laerte <38473563+laertecardozo@users.noreply.github.com>
2024-01-11 17:47:30 -0300

    Add computer choice

--- javascript.js
+++ javascript.js
@@ -1,5 +1,12 @@
 function getComputerChoice() {
-
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    if(randomNumber === 1) {
+        return "rock"; 
+    } else if(randomNumber === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
 
 function playRound(playerSelection, computerSelection) {
