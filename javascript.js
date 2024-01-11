9a0786eaba7f97785b9b6ee8f1c53e794a250f7c
Laerte <38473563+laertecardozo@users.noreply.github.com>
2024-01-11 17:33:20 -0300

    Add user input

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+
+}
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+const playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
